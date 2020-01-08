c293d3acee8ff40be43c6849605b1d83d53ff4f8
Tsuneko Shirley Kogami <tsuneko@twindoll.me>
2020-01-09 04:06:40 +1100

    Update generate_commands.py

--- Mudae/generate_commands.py
+++ Mudae/generate_commands.py
@@ -1,29 +1,47 @@
-f = open("waifulist.txt","r", encoding="utf-8")
-w = open("waifulist_commands.txt", "w", encoding="utf-8")
-waifus = []
-for line in f.readlines():
+waifulistFile = open("waifulist.txt","r", encoding="utf-8")
+waifulist = []
+for line in waifulistFile.readlines():
     l = line.rstrip()
-    li = 0
     if '-' in l:
         l = l.split(' - ')[0]
-    if l == "":
-        break
-    waifus.append(l)
+    waifulist.append(l)
+waifulistFile.close()
 
+mmsFile = open("mms_output.txt", "r", encoding="utf-8")
+ownedlistFile = open("ownedlist.txt", "w", encoding="utf-8")
+ownedwaifulist = []
+ownedwaifuranks = []
+for line in mmsFile.readlines():
+    l = line.rstrip()
+    if ' | ' in l:
+        l = l.split(' | ')[0]
+    if l in waifulist:
+        ownedlistFile.write(l+"\n")
+        ownedwaifulist.append(l)
+        ownedwaifuranks.append(waifulist.index(l))
+ownedlistFile.close()
+mmsFile.close()
 
-for i in range(len(waifus)//5):
-    w.write("$l ")
+commandsFile = open("commands.txt", "w", encoding="utf-8")
+
+commandsFile.write("Like List\n")
+for i in range(len(waifulist)//5):
+    commandsFile.write("$l ")
     for j in range(5):
         if j < 4:
-            w.write(waifus[i*5+j] + "$")
+            commandsFile.write(waifulist[i*5+j] + "$")
         else:
-            w.write(waifus[i*5+j] + "\n")
+            commandsFile.write(waifulist[i*5+j] + "\n")
+commandsFile.write("\n\n")
 
-w.write("\n\n")
+commandsFile.write("Notes\n")
+for i in range(len(waifulist)):
+    if waifulist[i] in ownedwaifulist:
+        commandsFile.write("$note " + waifulist[i] + "$Rank " + str(i+1) + "\n")
+commandsFile.write("\n\n")
 
-for i in range(len(waifus)):
-    w.write("$note " + waifus[i] + "$Rank " + str(i+1) + "\n")
+commandsFile.write("Sort\n")
+commandsFile.write("$sm note " + "$".join(list(map(lambda x: "Rank " + str(x+1), ownedwaifuranks))))  
+commandsFile.close()
 
-    
-f.close()
-w.close()
+print("Done.")
