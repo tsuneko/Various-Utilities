7cb79e4cbccf0469c512693c91626521a084a309
Tsuneko Shirley Kogami <tsuneko@twindoll.me>
2020-02-28 17:35:29 +1100

    Update botprofilenamegenerator.py

--- csgo/botprofilenamegenerator.py
+++ csgo/botprofilenamegenerator.py
@@ -3,28 +3,30 @@ import random
 w = open("names.txt", "r")
 names = []
 for line in w.readlines():
-    names.append(line.rstrip())
-    print(line.rstrip())
+	names.append(line.rstrip())
 w.close()
 
 c = 1
 
 x = open("botprofile_names.db", "w")
 for line in open("botprofile_base.db", "r").readlines():
-    x.write(line)
+	x.write(line)
 
-botDifficulties = ["Expert", "Hard", "Normal", "Easy"]
-botDifficultyIndex = 0
+diff = ["Expert", "Hard", "Normal", "Easy"]
+diffI = 0
 for i in range(len(names)):
-    if botDifficultyIndex < 3 and i%(len(names)//4)==0:
-        print(i)
-        botDifficultyIndex += 1
-    if i%(len(names)//8)==0:
-        weapon = "Sniper"
+    if i > 0 and diffI < 3 and i%(len(names)//4)==0:
+        print("---")
+        diffI += 1
+    if i > 0 and i%((len(names)//4)//2)==0:
+    	weapon = "Sniper"
     else:
-        weapon = "Rifle"
-    x.write(botDifficulties[botDifficultyIndex] + "+" + weapon + " " + names[i] + "\n")
+    	weapon = "Rifle"
+    x.write(diff[diffI] + "+" + weapon + " " + names[i] + "\n")
+    print("Creating bot: " + names[i] + " (Difficulty: " + diff[diffI] + ", Weapon: " + weapon + ")")
     x.write("	VoicePitch = " + str(random.randrange(90,110)) + "\n")
     x.write("End\n\n")
 
 x.close()
+
+print("---\nDone.")
